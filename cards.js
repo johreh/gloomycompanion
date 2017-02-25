8f0225924e3264cb6c4dfc5d206aab6d8d0596d3
JR <jrehnman@gmail.com>
2017-02-25 08:10:19 +0100

    Added resources for creating dynamic cards, updated css rules.

--- cards.js
+++ cards.js
@@ -3,20 +3,12 @@ DECK_DEFINITONS =
         , backside: "images/living_bones.png"
         , cards:
             [ ["images/living_bones_20.png", 1, false]
-            , ["images/living_bones_25.png", 1, false]
-            , ["images/living_bones_45.png", 2, false]
-            , ["images/living_bones_64.png", 1, false]
-            , ["images/living_bones_81.png", 1, false]
             ]
         },
         { name: "Guard"
         , backside: "images/guard.png"
         , cards:
             [ ["images/guard_15.png", 1, false]
-            , ["images/guard_30.png", 1, false]
-            , ["images/guard_35.png", 1, false]
-            , ["images/guard_50.png", 2, false]
-            , ["images/guard_70.png", 1, false]
             ]
         }
     ]
