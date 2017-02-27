d004ce4e8e5e546ead9769b278dd49e3629755c2
Gino <luism@Ginos-Mac.local>
2017-02-27 15:30:14 +0100

    Added Imp, Ooze and Rending drake, and fixed some spelling errors. Completed the list of the missing monsters.

--- cards.js
+++ cards.js
@@ -1,10 +1,23 @@
 // Taking the examples from http://imgur.com/a/S2sUn
 // TODO: Monster missing which need something extra
-//      Savvas Icestorm: Negate element 
-//      Spitting Drake: AOE attack icon
+//      Savvas Lavaflow: Negate element, line AOE
+//      Savvas Icestorm: Negate element, big triangle AOE
+//      Spitting Drake: Triangle AOE attack icon
 //      Cultist: surrounding attack icon
 //      Deep terror: Long line attack icon
 //      Harrower Infester: Short line attack icon
+//      Living Spirit: Negate element
+//      Lurker: Negate element
+//      Night Demon: Negate element
+//      Ancient Artillery: Triangle AOE, circle AOE
+//      Earth Demon: Negate element, 4 hit AOE
+//      Flame Demon: Negate element, circle AOE
+//      Frost Demon: Negate element, triangle AOE
+//      Wind Demon: Negate element, 4 hit AOE, Circle AOE
+//      Sun Demon: Negate element
+
+
+
 DECK_DEFINITONS = 
     [   { name: "Living Bones"
         , cards:
@@ -116,11 +129,11 @@ DECK_DEFINITONS =
         },
         { name: "Giant Viper"
         , cards:
-            [ [true,  "32", "* %move% +0", "* %attack% +0", "*** Add +2 %attack% if the target is adjacent", "*** to any of the Giant Viper's allies"]
-            , [true,  "32", "* %move% +0", "* %attack% +0", "*** Add +2 %attack% if the target is adjacent", "*** to any of the Giant Viper's allies"]
+            [ [true,  "32", "* %move% +0", "* %attack% +0", "*** Add +2 %attack% if the target is adjacent to any of the Giant Viper's allies"]
+            , [true,  "32", "* %move% +0", "* %attack% +0", "*** Add +2 %attack% if the target is adjacent to any of the Giant Viper's allies"]
             , [false, "43", "* %move% +1", "** %jump%", "* %attack% +1", "** Target all adjacent enemies"]
             , [false, "11", "* %shield% 1", "* %attack% -1"]
-            , [false, "58", "* %move% +1", "** %jump%", "* %attack% +1", "** All attacks targeting Giant Viper", "** this round gain Disadvantage"]
+            , [false, "58", "* %move% +1", "** %jump%", "* %attack% +1", "** All attacks targeting Giant Viper this round gain Disadvantage"]
             , [false, "58", "* %move% -1", "* %attack% +1"]
             , [false, "23", "* %move% -1", "* %attack% -1", "** %immobilize%", "* %attack% -1"]
             , [false, "43", "* %move% -1", "** %jump%", "* %attack% +0", "** %target% 2"]
@@ -130,12 +143,48 @@ DECK_DEFINITONS =
         , cards:
             [ [false, "06", "* %move% -1", "* %attack% +0", "** %immobilize%"]
             , [false, "07", "* %move% +0", "* %muddle%", "** Target all adjacent enemies"]
-            , [true, "19", "* %move% +1", "* %attack% +0", "*** Add +2 %attack% if the target is adjacent", "*** to any of the Hound's allies"]
-            , [true, "19", "* %move% +1", "* %attack% +0", "*** Add +2 %attack% if the target is adjacent", "*** to any of the Hound's allies"]
+            , [true,  "19", "* %move% +1", "* %attack% +0", "*** Add +2 %attack% if the target is adjacent to any of the Hound's allies"]
+            , [true,  "19", "* %move% +1", "* %attack% +0", "*** Add +2 %attack% if the target is adjacent to any of the Hound's allies"]
             , [false, "26", "* %move% +0", "* %attack% +0"]
             , [false, "26", "* %move% +0", "* %attack% +0"]
             , [false, "83", "* %move% -2", "* %attack% +1"]
             , [false, "72", "* %attack% -1", "** %pierce% 2", "* %move% -2", "* %attack% -1", "** %pierce% 2"]
+            ]
+        },
+        { name: "Imp"
+        , cards:
+            [ [false, "05", "* %shield% 5", "* %heal% 1", "** Self"]
+            , [false, "37", "* %move% +0", "* %attack% +0"]
+            , [false, "37", "* %move% +0", "* %attack% +0"]
+            , [false, "42", "* %move% +1", "* %heal% 2"]
+            , [true,  "43", "* %move% +0", "* %attack% -1", "** %target% 2", "** %poison%"]
+            , [false, "76", "* %move% -1", "* %attack% +1"]
+            , [true,  "43", "* %move% +0", "* %attack% -1", "** %target% 2", "** %curse%"]
+            , [false, "24", "* %strengthen% -1", "** Affect all allies within %range% 2", "* %muddle%", "** Target all enemies within %range% 2"]
+            ]
+        },
+        { name: "Ooze"
+        , cards:
+            [ [false, "36", "* %move% +1", "* %attack% -1"]
+            , [false, "57", "* %move% +0", "* %attack% 0"]
+            , [false, "59", "* %attack% +0", "** %target% 2", "** %poison%"]
+            , [false, "66", "* %move% -1", "* %attack% +1", "** %range% +1"]
+            , [true,  "94", "* Ooze suffers 2 damage ", "** Summons normal Ooze with a hit point value equal to the summoning Ooze's current hit point value (limited by a normal Ooze's specified maximum hit point value)"]
+            , [true,  "94", "* Ooze suffers 2 damage ", "** Summons normal Ooze with a hit point value equal to the summoning Ooze's current hit point value (limited by a normal Ooze's specified maximum hit point value)"]
+            , [false, "85", "* %push% 1 and", "* %poison%", "** Target all adjacent enemies", "* %attack% +1", "** %range% -1"]
+            , [false, "66", "* %move% -1", "* %loot% 1", "* %heal% 2", "** Self"]
+            ]
+        },
+        { name: "Rending Drake"
+        , cards:
+            [ [false, "12", "* %move% +1", "* %attack% -1"]
+            , [true,  "13", "* %attack% -1", "* %move% -1", "* %attack% -1"]
+            , [false, "25", "* %move% +0", "* %attack% +0"]
+            , [false, "39", "* %move% -1", "* %attack% +1"]
+            , [false, "54", "* %move% -2", "* %attack% -1", "** %range% 3", "** %target% 2", "** %poison%"]
+            , [false, "59", "* %move% -2", "* %attack% +1", "** %target% 2"]
+            , [false, "06", "* %shield% 2", "* %heal% 2", "** Self", "* %strengthen%", "** Self"]
+            , [true,  "72", "* %attack% -1", "* %attack% -1", "* %attack% -2"]
             ]
         }
     ];
