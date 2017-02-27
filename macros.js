63a0ec50ad61a00c0f63a21a812b740fd5ced74e
JR <jrehnman@gmail.com>
2017-02-27 23:39:29 +0100

    Added remaining macros used by monsters

--- macros.js
+++ macros.js
@@ -1,13 +1,21 @@
 
 /* Macros used in card text, alphabetical order */
 MACROS =
-    { "%attack%":       "<span class='nobr'>Attack <img class='icon' src='images/attack.svg'></span>"
+    { "%air%":          "<img class='element icon' src='images/air.svg'>"
+    , "%any%":          "<img class='element icon' src='images/any_element.svg'>"
+    , "%attack%":       "<span class='nobr'>Attack <img class='icon' src='images/attack.svg'></span>"
     , "%bless%":        "<span class='nobr'>BLESS <img class='icon' src='images/bless.svg'></span>"
     , "%curse%":        "<span class='nobr'>CURSE <img class='icon' src='images/curse.svg'></span>"
+    , "%dark%":         "<img class='element icon' src='images/dark.svg'>"
     , "%disarm%":       "<span class='nobr'>DISARM <img class='icon' src='images/disarm.svg'></span>"
+    , "%earth%":        "<img class='element icon' src='images/earth.svg'>"
+    , "%fire%":         "<img class='element icon' src='images/fire.svg'>"
     , "%heal%":         "<span class='nobr'>Heal <img class='icon' src='images/heal.svg'></span>"
+    , "%ice%":          "<img class='element icon' src='images/ice.svg'>"
     , "%immobilize%":   "<span class='nobr'>IMMOBILIZE <img class='icon' src='images/immobilize.svg'></span>"
+    , "%invisible%":    "<span class='nobr'>INVISIBLE <img class='icon' src='images/invisibility.svg'></span>"
     , "%jump%":         "<span class='nobr'>Jump <img class='icon' src='images/jump.svg'></span>"
+    , "%light%":        "<img class='element icon' src='images/light.svg'>"
     , "%loot%":         "<span class='nobr'>Loot <img class='icon' src='images/loot.svg'></span>"
     , "%move%":         "<span class='nobr'>Move <img class='icon' src='images/move.svg'></span>"
     , "%muddle%":       "<span class='nobr'>MUDDLE <img class='icon' src='images/muddle.svg'></span>"
@@ -19,9 +27,10 @@ MACROS =
     , "%retaliate%":    "<span class='nobr'>Retaliate <img class='icon' src='images/retaliate.svg'></span>"
     , "%shield%":       "<span class='nobr'>Shield <img class='icon' src='images/shield.svg'></span>"
     , "%strengthen%":   "<span class='nobr'>STRENGTHEN <img class='icon' src='images/strengthen.svg'></span>"
+    , "%stun%":         "<span class='nobr'>STUN <img class='icon' src='images/stun.svg'></span>"
     , "%target%":       "<span class='nobr'>Target <img class='icon' src='images/target.svg'></span>"
+    , "%use_element%":  "<img class='element icon overlay' src='images/use_element.svg'>"
     , "%wound%":        "<span class='nobr'>WOUND <img class='icon' src='images/wound.svg'></span>"
-    , "%use_element%":  "<img class='icon overlay' src='images/use_element.svg'>"
     };
 
 function expand_macro(macro)
